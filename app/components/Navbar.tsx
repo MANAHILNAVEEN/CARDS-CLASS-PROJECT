134778b3eb0331fb58198f46810463cd960ab1fb
manahil <sufyanhingoro27@gmail.com>
2024-10-19 20:39:59 +0500

    first commit

---
+++ app/components/Navbar.tsx
@@ -0,0 +1,17 @@
+export default function (){
+    return(
+        <div>
+        <header>
+         <nav className="flex items-center w-full bg-black h-32">
+         <div className="bg-blue-800 w-96 h-10"></div>
+         
+         <div className="flex justify-end w-3/5 items-center">
+         <div className="bg-pink-400 w-44 h-10"></div>
+         <div className="bg-yellow-300 w-44 h-10"></div>
+         <div className="bg-purple-400 w-44 h-10"></div>
+         </div>
+         </nav>
+         </header>
+         </div>
+    );
+}
